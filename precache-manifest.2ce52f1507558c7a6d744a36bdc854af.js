8f824d9b459bca24029e44746fa4b9d679306694
Vinicius Pereira <viniciusogp@gmail.com>
2020-07-13 09:21:27 -0300

    Updates

---
+++ precache-manifest.2ce52f1507558c7a6d744a36bdc854af.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c7c89685f7796a2fa3fcfee6ac48f21",
+    "url": "/liberteofuturo-react/index.html"
+  },
+  {
+    "revision": "8aee7d31d808dc77feab",
+    "url": "/liberteofuturo-react/static/css/2.6c27cde9.chunk.css"
+  },
+  {
+    "revision": "74e557309617503736b1",
+    "url": "/liberteofuturo-react/static/css/main.dc895e6a.chunk.css"
+  },
+  {
+    "revision": "8aee7d31d808dc77feab",
+    "url": "/liberteofuturo-react/static/js/2.174fafd2.chunk.js"
+  },
+  {
+    "revision": "87e7d156e76bf0ee9b32832aeb012217",
+    "url": "/liberteofuturo-react/static/js/2.174fafd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74e557309617503736b1",
+    "url": "/liberteofuturo-react/static/js/main.7819d82a.chunk.js"
+  },
+  {
+    "revision": "07ce7061933eff049d89",
+    "url": "/liberteofuturo-react/static/js/runtime-main.3f615cb3.js"
+  },
+  {
+    "revision": "e95fab9f6bf8b881942506dbd8bf7c07",
+    "url": "/liberteofuturo-react/static/media/back.e95fab9f.mp4"
+  },
+  {
+    "revision": "75e4d7f000f816b591416cf5d8599bf1",
+    "url": "/liberteofuturo-react/static/media/capa1.75e4d7f0.jpg"
+  },
+  {
+    "revision": "7a19298b2646472f80734e54228f0ea9",
+    "url": "/liberteofuturo-react/static/media/fundo.7a19298b.svg"
+  },
+  {
+    "revision": "bd4d1e00fa80b96a2e29408d0590ddb3",
+    "url": "/liberteofuturo-react/static/media/logo_vertical.bd4d1e00.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/liberteofuturo-react/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/liberteofuturo-react/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/liberteofuturo-react/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/liberteofuturo-react/static/media/slick.f97e3bbf.svg"
+  }
+]);
